3d8d7df58cee64071ed582bf5c42210d93118e3c
Lucas Gonçalves Cordeiro <202119060310@lab02-l>
2023-04-17 16:39:38 -0400

    Updated the hardcoded html pages with style link

--- Server/HTTP_Server.py
+++ Server/HTTP_Server.py
@@ -5,7 +5,7 @@ CRLF = '\r\n'
 
 def replyNotFoundRequest(socket):
     httpHeader = 'HTTP/1.1 404 Not Found' + CRLF + CRLF
-    htmlNotFound = '<!DOCTYPE html><html><head><title>404 - Not Found</title></head><body><h1>Error 404 - Not Found</h1></body></html>'
+    htmlNotFound = '<!DOCTYPE html><html><head><link rel="stylesheet" href="/style/error.css"><title>404 - Not Found</title></head><body><h1>Error 404 - Not Found</h1></body></html>'
     socket.send(httpHeader.encode())
     socket.send(htmlNotFound.encode())
     socket.send(''.encode())
@@ -13,7 +13,7 @@ def replyNotFoundRequest(socket):
 
 def replyBadRequest(socket):
     httpHeader = 'HTTP/1.1 400 Bad Request' + CRLF + CRLF
-    htmlBadRequest = '<!DOCTYPE html><html><head><title>400 - Bad Request</title></head><body><h1>Error 400 - Bad Request</h1></body></html>'
+    htmlBadRequest = '<!DOCTYPE html><html><head><link rel="stylesheet" href="/style/error.css"><title>400 - Bad Request</title></head><body><h1>Error 400 - Bad Request</h1></body></html>'
     socket.send(httpHeader.encode())
     socket.send(htmlBadRequest.encode())
     socket.send(''.encode())
@@ -21,7 +21,7 @@ def replyBadRequest(socket):
 
 def replyNotImplemented(socket):
     httpHeader = 'HTTP/1.1 501 Not Implemented' + CRLF + CRLF
-    htmlNotImplemented = '<!DOCTYPE html><html><head><title>501 - Not Implemented</title></head><body><h1>Error 501 - Not Implemented</h1></body></html>'
+    htmlNotImplemented = '<!DOCTYPE html><html><head><link rel="stylesheet" href="/style/error.css"><title>501 - Not Implemented</title></head><body><h1>Error 501 - Not Implemented</h1></body></html>'
     socket.send(httpHeader.encode())
     socket.send(htmlNotImplemented.encode())
     socket.send(''.encode())
